3a74d84ecfcfef335a3b13dfd0cc7590b0128a3a
Luis Carvajal <luisda7cr@gmail.com>
2019-04-11 16:28:11 -0600

    test
    Some problems with operators

--- Fase3/Emisor.cpp
+++ Fase3/Emisor.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <iostream>
 #include <stdio.h>
+#include <sstream>
 #include <queue>
 #include <stdlib.h>
 #include "Mailbox.h"
@@ -13,11 +14,27 @@
 #include <vector>
 #include <pthread.h>
 
+//------------------Server---------------------
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+
+#include <sys/socket.h> // Core BSD socket functions and data structures.
+#include <netinet/in.h> // AF_INET and AF_INET6 address families and their
+                        // corresponding protocol families PF_INET and PF_INET6.
+#include <arpa/inet.h>  // Functions for manipulating numeric IP addresses.
+
+#define MAX 80 
+#define PORT 8080 
+#define SA struct sockaddr 
+//------------------Server---------------------
+
 using namespace std;
 
 typedef struct threadData{
 	
-	queue <vector<char>> data;
+	queue < vector< char > > data;
 	int id;
 	string name;
 	long totalBytes;
@@ -82,6 +99,58 @@ void* child(void* data)
 
 int main()
 {	
+	//------------------Server---------------------
+	int sockfd, connfd, len; 
+	struct sockaddr_in servaddr, cli; 
+
+	// socket create and verification 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	if (sockfd == -1) { 
+		printf("socket creation failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully created..\n"); 
+	bzero(&servaddr, sizeof(servaddr)); 
+
+	// assign IP, PORT 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); // Get IP
+	servaddr.sin_port = htons(PORT);  // Get Port
+	// Binding newly created socket to given IP and verification 
+	if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
+		printf("socket bind failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully binded..\n"); 
+
+	// Now server is ready to listen and verification 
+	if ((listen(sockfd, 5)) != 0) { 
+		printf("Listen failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Server listening..\n"); 
+	len = sizeof(cli); 
+
+	// Accept the data packet from client and verification 
+	connfd = accept(sockfd, (SA*)&cli, &len); 
+	if (connfd < 0) { 
+		printf("server acccept failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("server acccept the client...\n"); 
+
+	// Function for chatting between client and server 
+	//func(connfd); 
+
+	// After chatting close the socket 
+	close(sockfd);
+	//------------------Server---------------------
+
+
 	//Creates the mailboxes
 	key_t key = 0xa62074;
 	Mailbox handshakeMail = Mailbox(key);
